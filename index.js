d9dd09faacfc8805f9036f1c9b4bf8c31c4c8af8
Joel Lisenby <joel.lisenby@gmail.com>
2018-08-16 12:55:14 -0700

    Update output
    Output now sorts the urls and also generates a table of contents.

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ module.exports = (options) => {
   const configPath = path.resolve(options.config)
   const config = JSON.parse(fs.readFileSync(configPath))
 
+  const output_destination = config.settings.output.destination || 'lightcrawler_output.html';
+
   const crawler = new Crawler(options.url)
   crawler.respectRobotsTxt = false
   crawler.parseHTMLComments = false
@@ -38,15 +40,22 @@ module.exports = (options) => {
   }
 
   let totalErrorCount = 0
+
+  let html_out_obj = {}
+
   let html_out = '<html lang="en_US"><head><title>Lighthouse Report</title>';
-  html_out += '<style>body { font-family: roboto; font-size: 14px; } a { color: #1a6dd8; } a:hover { color: #0c4896 } a:visited { color: #1a6dd8; } h1,h2 { padding: 1em 0; } h1 { font-size: 1.5em; } h2 { font-size: 1.25em; } .audit-url-result { padding: 2em 0; border-bottom: 1px solid #efefef; } .audit-node-messages ul li ul { margin: 0; padding: 0; list-style: none; } .audit-node-messages ul li ul li { display: block; font-family: "droid sans mono"; width: fit-content; font-size: 0.8em; list-style-type: none; margin: 2px 0; padding: 5px; background-color: #efefef; }</style>';
+  html_out += '<style>body { font-family: roboto; font-size: 14px; } a { color: #1a6dd8; } a:hover { color: #0c4896 } a:visited { color: #1a6dd8; } h1,h2 { padding: 1em 0; } h1 { font-size: 1.5em; } h2 { font-size: 1.25em; } .audit { padding-bottom: 1em; } .audit-url-result { padding: 2em 0; border-bottom: 4px solid #b7b7b7; } .audit-node-messages ul li ul { margin: 0; padding: 0; list-style: none; } .audit-node-messages ul li ul li { list-style-type: none; margin: 2px 0; padding: 0; } code { display: inline-block; font-family: "droid sans mono"; font-size: 0.8em; padding: 5px; background-color: #efefef; }</style>';
   html_out += '</head><body>';
   html_out += '<h1>Accessibility Scan Results</h1>';
 
   const lighthouseQueue = queue((url, callback) => {
-    runLighthouse(url, configPath, (errorCount, lh_html_out) => {
+    runLighthouse(url, configPath, (errorCount, lh_html_out_obj) => {
       totalErrorCount += errorCount
-      html_out += lh_html_out;
+
+      Object.keys(lh_html_out_obj).forEach(function(key) {
+        html_out_obj[key] = lh_html_out_obj[key]
+      })
+
       callback()
     })
   }, config.settings.crawler.maxChromeInstances)
@@ -57,10 +66,27 @@ module.exports = (options) => {
   crawler.once('complete', () => {
     lighthouseQueue.drain = () => {
       printStats()
+      
+      // sort html_out_obj by key
+      var html_out_obj_sorted = {};
+      Object.keys(html_out_obj).sort().forEach(function(key) {
+        html_out_obj_sorted[key] = html_out_obj[key];
+      })
+
+      
+      html_out += '<div class="table-of-contents"><h2 id="table-of-contents">Table of Contents</h2><ul>';
+      Object.keys(html_out_obj_sorted).forEach(function(key) {
+        html_out += '<li><a href="#'+ key +'">'+ key +'</a></li>';
+      })
+      html_out += '</ul></div>';
+
+      Object.keys(html_out_obj_sorted).forEach(function(key) {
+        html_out += html_out_obj_sorted[key];
+      })
 
       html_out += '</body></html>';
       
-      fs.writeFileSync('result.html', html_out);
+      fs.writeFileSync(output_destination, html_out);
 
       if (totalErrorCount > 0) {
         process.exit(1)
@@ -86,7 +112,6 @@ function runLighthouse (url, configPath, callback) {
 
   const lighthousePath = require.resolve('lighthouse/lighthouse-cli/index.js')
   const lighthouse = ChildProcess.spawn(lighthousePath, args)
-
   let output = ''
   lighthouse.stdout.on('data', (data) => {
     output += data
@@ -96,7 +121,8 @@ function runLighthouse (url, configPath, callback) {
   lighthouse.once('close', () => {
     stats.auditTimesByPageUrl[url].endTime = new Date()
     let errorCount = 0
-    let lh_html_out = '';
+    let lh_html_out_obj = {}
+    lh_html_out_obj[url] = '';
 
     let report
     try {
@@ -107,7 +133,7 @@ function runLighthouse (url, configPath, callback) {
       return
     }
     
-    lh_html_out += '<div class="audit-url-result"><h2 class="audit-url"><a href="'+ url +'">'+ url +'</a></h2>';
+    lh_html_out_obj[url] += '<div class="audit-url-result"><h2 id="'+ url +'" class="audit-url"><a href="'+ url +'">'+ url +'</a></h2>';
 
     report.reportCategories.forEach((category) => {
       let displayedCategory = false
@@ -118,16 +144,16 @@ function runLighthouse (url, configPath, callback) {
           stats.passedAuditsCount++
         } else {
 
-          lh_html_out += '<div class="audit">';
+          lh_html_out_obj[url] += '<div class="audit">';
 
           if (!displayedCategory) {
-            console.log();
-            console.log(category.name.bold.underline);
+            //console.log();
+            //console.log(category.name.bold.underline);
             displayedCategory = true
           }
           errorCount++
-          console.log(url.replace(/\/$/, ''), '\u2717'.red, audit.id.bold, '-', audit.result.description.italic)
-          lh_html_out += '<div class="audit-description"><p><strong>'+ audit.id +'</strong> - '+ entities.encode( audit.result.description ) +'</p></div>';
+          //console.log(url.replace(/\/$/, ''), '\u2717'.red, audit.id.bold, '-', audit.result.description.italic)
+          lh_html_out_obj[url] += '<div class="audit-description"><p><strong>'+ audit.id +'</strong> - '+ entities.encode( audit.result.description ) +'</p></div>';
 
           if (stats.violationCounts[category.name] === undefined) {
             stats.violationCounts[category.name] = 0
@@ -136,16 +162,16 @@ function runLighthouse (url, configPath, callback) {
           if (audit.result.extendedInfo) {
             const {value} = audit.result.extendedInfo
             if (Array.isArray(value)) {
-              lh_html_out += '<div class="extended-info"><h3>Extended Info</h3>';
-              lh_html_out += '<ul>';
+              lh_html_out_obj[url] += '<div class="extended-info"><h3>Extended Info</h3>';
+              lh_html_out_obj[url] += '<ul>';
               stats.violationCounts[category.name] += value.length
               value.forEach((result) => {
                 if (result.url) {
-                  lh_html_out += '<li>'+ result.url +'</li>';
-                  console.log(`   ${result.url}`)
+                  lh_html_out_obj[url] += '<li>'+ result.url +'</li>';
+                  //console.log(`   ${result.url}`)
                 }
               })
-              lh_html_out += '</ul></div>';
+              lh_html_out_obj[url] += '</ul></div>';
             } else if (Array.isArray(value.nodes)) {
               stats.violationCounts[category.name] += value.nodes.length
               const messagesToNodes = {}
@@ -158,33 +184,33 @@ function runLighthouse (url, configPath, callback) {
                   messagesToNodes[message] = [result.html]
                 }
               })
-              lh_html_out += '<div class="audit-node-messages"><ul>';
+              lh_html_out_obj[url] += '<div class="audit-node-messages"><ul>';
               Object.keys(messagesToNodes).forEach((message) => {
-                console.log(`   ${message}`)
-                lh_html_out += '<li><strong>'+ nl2br(message) +'.</strong>';
-                lh_html_out += '<ul>';
+                //console.log(`   ${message}`)
+                lh_html_out_obj[url] += '<li><strong>'+ nl2br(message) +'.</strong>';
+                lh_html_out_obj[url] += '<ul>';
                 messagesToNodes[message].forEach(node => {
-                  console.log(`     ${node}`.gray)
-                  lh_html_out += '<li>'+ entities.encode(node) +'</li>';
+                  //console.log(`     ${node}`.gray)
+                  lh_html_out_obj[url] += '<li><code>'+ entities.encode(node) +'</code></li>';
                 })
-                lh_html_out += '</ul>'
-                lh_html_out += '</li>';
+                lh_html_out_obj[url] += '</ul>'
+                lh_html_out_obj[url] += '</li>';
               })
-              lh_html_out += '</ul></div>';
+              lh_html_out_obj[url] += '</ul></div>';
             } else {
               stats.violationCounts[category.name]++
             }
           }
           
-          lh_html_out += '</div>'; // .audit
+          lh_html_out_obj[url] += '</div>'; // .audit
 
         }
       })
     })
 
-    lh_html_out += '</div>'; // .audit-url-result
+    lh_html_out_obj[url] += '</div>'; // .audit-url-result
 
-    callback(errorCount, lh_html_out)
+    callback(errorCount, lh_html_out_obj)
   })
 }
 
